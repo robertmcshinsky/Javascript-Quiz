04e6a94bf55305f6dc56571deef7ab5ef9d34b0a
Robert McShinsky <robert.mcshinsky@gmail.com>
2021-02-14 03:19:52 -0700

    Main html and css and javascript info

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//TODO: Create Quiz Database
+//TODO: Create Program that randomizes and places new html content into the web page
+
+//TODO: Create HIGH SCORE Data base - with a user input section for initials.
+
+//? Create Function for randomizing questions with arrays
+//? Create Function for logging and storing user score data with arrays
+//? Create Function for Displaying the high scores with arrays
+//? Create User interaction section for user clicks
+//? Create time countdown and keep this for part of the score
+//? Create time subtracting from clock
+//? GAME OVER initialization
