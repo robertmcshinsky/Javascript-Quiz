66ef8927ab27987a7cdcd873d105f975eca865c8
Robert McShinsky <robert.mcshinsky@gmail.com>
2021-02-22 00:13:08 -0700

    Added quiz questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,115 @@ let QA = {
     answer: 'document.write("Hello World")',
   },
   2: {
-    question: "Where?",
-    answer: "Correct3",
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: "The head section and the body section ",
   },
   3: {
-    question: "When?",
-    answer: "Correct4",
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    answer: 'script src="xxx.js',
   },
   4: {
-    question: "Why?",
-    answer: "Correct5",
+    question: 'How do you write "Hello World" in an alert box?',
+    answer: 'alert("Hello World")',
+  },
+  5: {
+    question: "How do you create a function?",
+    answer: "function myFunction()",
+  },
+  6: {
+    question:
+      'How do you write a conditional statement for executing some statements only if "i" is equal to 5?',
+    answer: "if (i==5)",
+  },
+  7: {
+    question: 'How do you call a function named "myFunction"?',
+    answer: "myFunction()",
+  },
+  8: {
+    question:
+      'How do you write a conditional statement for executing some statements only if "i" is NOT equal to 5?',
+    answer: "if (i != 5)",
+  },
+  9: {
+    question: "How many looping statements are there in JavaScript?",
+    answer: 'The "for" loop, the "while" loop, and the "do...while" loop',
+  },
+  10: {
+    question: 'How does a "for" loop start?',
+    answer: "for (i = 0; i <= 5; i++)",
+  },
+  11: {
+    question: "How can you add a comment in a JavaScript?",
+    answer: "//This is a comment",
+  },
+  12: {
+    question:
+      "What is the correct JavaScript syntax to insert a comment that has more than one line?",
+    answer: "/*This comment has more than one line*/",
+  },
+  13: {
+    question: "What is the correct way to write a JavaScript array?",
+    answer: 'let txt = new Array("tim","kim","jim")',
+  },
+  14: {
+    question: "How do you round the number 7.25, to the nearest whole number?",
+    answer: "Math.round(7.25)",
+  },
+  15: {
+    question: "How do you find the largest number of 2 and 4?",
+    answer: "Math.max(2,4)",
+  },
+  16: {
+    question:
+      'What is the correct JavaScript syntax for opening a new window called "window2" ?',
+    answer: 'window.open("http://www.w3schools.com","window2")',
   },
 };
 let wrongAnswers = [
-  "Wrong1",
-  "Wrong2",
-  "Wrong3",
-  "Wrong4",
-  "Wrong5",
-  "Wrong6",
-  "Wrong7",
+  "javascript",
+  "js",
+  "scripting",
+  'response.write("Hello World")',
+  '"Hello World"',
+  '("Hello World")',
+  "The <body> section",
+  "The <head> section",
+  '<script name="xxx.js">',
+  '<script href="xxx.js">',
+  '<script value="xxx.js">',
+  'msgBox("Hello World")',
+  'alertBox="Hello World"',
+  'alertBox("Hello World")',
+  "function:myFunction()",
+  "function=myFunction()",
+  "myFunction():function",
+  "call myFunction()",
+  "call function myFunction",
+  "Call.myFunction()",
+  "if i==5 then",
+  "if i=5 then",
+  "if i=5",
+  "if =! 5 then",
+  "if (i <> 5)",
+  "if <>5",
+  '2. The "for" loop, and the "while" loop',
+  '4. The "for" loop, the "while" loop, the "do...while" loop, and the "loop...until" loop',
+  "This is a comment",
+  '"<!--This is a comment-->"',
+  "#This is a comment",
+  '"<!--This comment has more than one line-->"',
+  "//This comment has more than one line//",
+  "##This comment has ##more than one line",
+  'var txt = new Array(1:"tim",2:"kim",3:"jim")',
+  'var txt = new Array="tim","kim","jim"',
+  'var txt = new Array:1=("tim")2=("kim")3=("jim")',
+  "round(7.25)",
+  "rnd(7.25)",
+  "Math.rnd(7.25)",
+  "Math.ceil(2,4)",
+  "ceil(2,4)",
+  "top(2,4)",
 ];
 let initials = [];
 let score = [];
@@ -66,13 +155,14 @@ function displayQuiz() {
   }
   function displayAnswers(i) {
     console.log("displayAnswers");
-    let answer = "<h1 id='correct'>" + QA[i].answer + "<h1>";
+    let answer = "<h1 id='correct'>" + '"' + QA[i].answer + '"' + "<h1>";
     document.getElementById("midSection").innerHTML = answer;
     document.getElementById("correct").addEventListener("click", function () {
       myScore++;
       score[playerNum] = myScore;
       document.getElementById("correct").style.backgroundColor = "green";
       ++i;
+      timerEl.innerText = parseInt(timerEl.innerText) + 3;
       displayQuiz();
     });
   }
@@ -89,7 +179,7 @@ function displayQuiz() {
       let rnd = Math.floor(Math.random() * 3);
       let randNum = Math.floor(Math.random() * wrongAnswers.length);
       let fakeAnswer =
-        "<h1 id='fake" + k + "'>" + wrongAnswers[randNum] + "</h1>";
+        "<h1 id='fake" + k + "'>" + '"' + wrongAnswers[randNum] + '"' + "</h1>";
 
       if (rnd > 1) {
         aboveOrBelow = "beforeEnd";
